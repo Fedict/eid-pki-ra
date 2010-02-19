72d784bef99446ca8abcccbbe7db2e38588b8f29
jan.vdbergh <jan.vdbergh@a75cd9ec-122e-11df-83e8-2f775389245c>
2010-02-19 12:03:45 +0000

    Added header and javadoc.

--- trunk/eid-pki-ra-blm-ws/src/main/java/be/fedict/eid/pkira/ws/impl/EIDPKIRAServiceImpl.java
+++ trunk/eid-pki-ra-blm-ws/src/main/java/be/fedict/eid/pkira/ws/impl/EIDPKIRAServiceImpl.java
@@ -1,3 +1,19 @@
+/*	  
+ * eID PKI RA Project.
+ * Copyright (C) 2010 FedICT.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see
+ * http://www.gnu.org/licenses/.
+ */
 package be.fedict.eid.pkira.ws.impl;
 
 import javax.ejb.EJB;
@@ -6,17 +22,29 @@ import javax.jws.WebService;
 import be.fedict.eid.blm.model.ContractHandler;
 import be.fedict.eid.pkira.blm.ws.EIDPKIRAPortType;
 
-@WebService(endpointInterface="be.fedict.eid.pkira.blm.ws.EIDPKIRAPortType")
+/**
+ * Implementation of the public web service. This class delegates all
+ * invocations to a contract handler EJB.
+ * 
+ * @author Jan Van den Bergh
+ */
+@WebService(endpointInterface = "be.fedict.eid.pkira.blm.ws.EIDPKIRAPortType")
 public class EIDPKIRAServiceImpl implements EIDPKIRAPortType {
 
 	@EJB
 	private ContractHandler contractHandler;
-	
+
+	/**
+	 * Revoke certificate implementation.
+	 */
 	@Override
 	public String revokeCertificate(String request) {
 		return contractHandler.revokeCertificate(request);
 	}
 
+	/**
+	 * Sign certificate implementation.
+	 */
 	@Override
 	public String signCertificate(String request) {
 		return contractHandler.signCertificate(request);
