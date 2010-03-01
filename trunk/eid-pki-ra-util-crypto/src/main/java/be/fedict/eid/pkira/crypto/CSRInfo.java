25948721d5cd79529f971646034d0294fc4f07b0
jan.vdbergh <jan.vdbergh@a75cd9ec-122e-11df-83e8-2f775389245c>
2010-03-01 09:13:57 +0000

    Adding crypto library with cryptographic methods.

---
+++ trunk/eid-pki-ra-util-crypto/src/main/java/be/fedict/eid/pkira/crypto/CSRInfo.java
@@ -0,0 +1,39 @@
+/*
+ * eID PKI RA Project.
+ * Copyright (C) 2010 FedICT.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see
+ * http://www.gnu.org/licenses/.
+ */
+package be.fedict.eid.pkira.crypto;
+
+/**
+ * Information extracted from a CSR.
+ * 
+ * @author Jan Van den Bergh
+ */
+public class CSRInfo {
+
+	private final String subject;
+
+	public CSRInfo(String subject) {
+		this.subject = subject;
+	}
+
+	/**
+	 * Returns the subject (distinguished name) found in the CSR.
+	 * @return
+	 */
+	public String getSubject() {
+		return subject;
+	}	
+}
---
+++ trunk/eid-pki-ra-util-crypto/src/test/java/be/fedict/eid/pkira/crypto/CSRParserTest.java
@@ -0,0 +1,66 @@
+/*
+ * eID PKI RA Project.
+ * Copyright (C) 2010 FedICT.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see
+ * http://www.gnu.org/licenses/.
+ */
+package be.fedict.eid.pkira.crypto;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
+import java.security.Security;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * @author Jan Van den Bergh
+ */
+public class CSRParserTest {
+
+	@BeforeClass
+	public void setup() {
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+	}
+
+	@Test
+	public void testParseCSR() throws CryptoException {
+		CSRInfo csrInfo = CSRParser.parseCSR(TestConstants.VALID_CSR);
+
+		assertNotNull(csrInfo);
+		assertEquals(csrInfo.getSubject(), TestConstants.CSR_SUBJECT);
+	}
+	
+	@Test(expectedExceptions = CryptoException.class)
+	public void testParseCSRInvalidSignature() throws CryptoException {
+		CSRParser.parseCSR(TestConstants.INVALID_CSR);
+	}
+
+	@Test(expectedExceptions = CryptoException.class)
+	public void testParseIncompleteCSR() throws CryptoException {
+		CSRParser.parseCSR(TestConstants.VALID_CSR.substring(0, TestConstants.VALID_CSR.length() - 10));
+	}
+
+	@Test(expectedExceptions = CryptoException.class)
+	public void testParseEmptyCSR() throws CryptoException {
+		CSRParser.parseCSR("");
+	}
+
+	@Test(expectedExceptions = CryptoException.class)
+	public void testParseCertificateAsCSR() throws CryptoException {
+		CSRParser.parseCSR(TestConstants.VALID_CERTIFICATE);
+	}
+	
+	
+}
