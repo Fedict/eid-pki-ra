23b095e63783dc910e3ddce662eb393efdcb8e4d
bramb75 <bramb75@gmail.com>
2010-03-11 10:58:20 +0000

    Created domain, repositories and unit tests for Registration, CertificateDomain and User.

---
+++ eid-pki-ra-blm-model/src/main/java/be/fedict/eid/pkira/blm/model/domain/CertificateDomain.java
@@ -0,0 +1,110 @@
+/**
+ * eID PKI RA Project. 
+ * Copyright (C) 2010 FedICT. 
+ * 
+ * This is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License version 
+ * 3.0 as published by the Free Software Foundation. 
+ * 
+ * This software is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * Lesser General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/. 
+ */
+
+package be.fedict.eid.pkira.blm.model.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+/**
+ * @author Bram Baeyens
+ *
+ */
+@Entity
+@NamedQueries({
+	@NamedQuery(
+			name="findByName",
+			query="SELECT cd FROM CertificateDomain cd WHERE cd.name = :name"
+	), 
+	@NamedQuery(
+			name="findByDnExpression",
+			query="SELECT cd FROM CertificateDomain cd WHERE cd.dnExpression = :dnExpression"
+	)
+})
+public class CertificateDomain implements Serializable {
+
+	private static final long serialVersionUID = -4193917177011312256L;
+	
+	@Id @GeneratedValue
+	@Column(name="CERTIFICATE_DOMAIN_ID")
+	private Integer id;
+	@Column(name="CERTIFICATE_DOMAIN_NAME", nullable=false, unique=true)
+	private String name;
+	@Column(name="DN_EXPRESSION", nullable=false, unique=true)
+	private String dnExpression;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDnExpression() {
+		return dnExpression;
+	}
+	
+	public void setDnExpression(String dnExpression) {
+		this.dnExpression = dnExpression;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		} 
+		if (!(obj instanceof CertificateDomain)) {
+			return false;
+		}
+		CertificateDomain that = (CertificateDomain) obj;
+		return this.dnExpression.equals(that.dnExpression);
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 43)
+				.append(dnExpression)
+				.toHashCode();
+	}
+	
+	@Override
+	public String toString() {
+		return new StringBuilder("CertificateDomain[")
+				.append("name=").append(name)
+				.append(", dnExpression=").append(dnExpression)
+				.append(']').toString();
+	}
+
+}
---
+++ eid-pki-ra-blm-model/src/main/java/be/fedict/eid/pkira/blm/model/jpa/RegistrationRepositoryBean.java
@@ -0,0 +1,82 @@
+/**
+ * eID PKI RA Project. 
+ * Copyright (C) 2010 FedICT. 
+ * 
+ * This is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License version 
+ * 3.0 as published by the Free Software Foundation. 
+ * 
+ * This software is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * Lesser General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this software; if not, see 
+ * http://www.gnu.org/licenses/. 
+ */
+
+package be.fedict.eid.pkira.blm.model.jpa;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+
+import be.fedict.eid.pkira.blm.model.domain.Registration;
+import be.fedict.eid.pkira.blm.model.domain.RegistrationStatus;
+
+/**
+ * @author Bram Baeyens
+ *
+ */
+@Stateless
+@Name(RegistrationRepository.NAME)
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
+public class RegistrationRepositoryBean implements RegistrationRepository {
+
+	@In
+	private EntityManager entityManager;
+	
+	protected void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@Override
+	public List<Registration> findAllNewRegistrations() {
+		return findRegistrationsByStatus(RegistrationStatus.NEW);
+	}
+
+	@Override
+	public void persist(Registration registration) {
+		entityManager.persist(registration);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private List<Registration> findRegistrationsByStatus(RegistrationStatus status) {
+		return entityManager.createNamedQuery("findRegistrationsByStatus")
+				.setParameter("status", status)
+				.getResultList();
+	}
+
+	@Override
+	public void reject(Registration registration) {
+		updateStatus(registration, RegistrationStatus.REJECTED);
+	}
+	
+	@Override
+	public void confirm(Registration registration) {
+		updateStatus(registration, RegistrationStatus.CONFIRMED);
+	}
+	
+	private void updateStatus(Registration registration, RegistrationStatus status) {
+		entityManager.getReference(Registration.class, registration.getId());
+		registration.setStatus(status);
+	}
+
+}
