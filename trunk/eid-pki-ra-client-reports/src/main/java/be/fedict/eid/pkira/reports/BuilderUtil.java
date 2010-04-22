be09365fd1e13a77ef1ffe69b9c152381f69958c
jan.vdbergh <jan.vdbergh@a75cd9ec-122e-11df-83e8-2f775389245c>
2010-04-22 12:24:09 +0000

    PKIRA-75 - As an administrator, I can download a report
    Implementation of the report XSD and code generation and the report client API.

---
+++ trunk/eid-pki-ra-client-reports/src/main/java/be/fedict/eid/pkira/reports/BuilderUtil.java
@@ -0,0 +1,35 @@
+/*
+ * eID PKI RA Project.
+ * Copyright (C) 2010 FedICT.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see
+ * http://www.gnu.org/licenses/.
+ */
+package be.fedict.eid.pkira.reports;
+
+import java.math.BigInteger;
+
+import be.fedict.eid.pkira.generated.reports.ObjectFactory;
+import be.fedict.eid.pkira.generated.reports.SuccessFailureCountType;
+
+
+public class BuilderUtil {
+
+	public static SuccessFailureCountType createSuccessFailureCountType(int successes, int failures) {
+		SuccessFailureCountType result = new ObjectFactory().createSuccessFailureCountType();
+		result.setSuccessCount(BigInteger.valueOf(successes));
+		result.setFailureCount(BigInteger.valueOf(failures));
+		result.setTotalCount(BigInteger.valueOf(successes+failures));
+		
+		return result;
+	}
+}
---
+++ trunk/eid-pki-ra-client-reports/src/main/java/be/fedict/eid/pkira/reports/ReportBuilder.java
@@ -0,0 +1,48 @@
+/*
+ * eID PKI RA Project.
+ * Copyright (C) 2010 FedICT.
+ * 
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see
+ * http://www.gnu.org/licenses/.
+ */
+package be.fedict.eid.pkira.reports;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import be.fedict.eid.pkira.generated.reports.ObjectFactory;
+import be.fedict.eid.pkira.generated.reports.ReportType;
+
+/**
+ * Builder to easily create ReportTypes.
+ * 
+ * @author Jan Van den Bergh
+ */
+public class ReportBuilder implements Builder<ReportType> {
+
+	private List<MonthlyReportBuilder> monthlyReportBuilders = new ArrayList<MonthlyReportBuilder>();
+	
+	public MonthlyReportBuilder newMonthlyReport() {
+		MonthlyReportBuilder result = new MonthlyReportBuilder();
+		monthlyReportBuilders.add(result);
+		return result;
+	}
+
+	public ReportType toXmlType() {
+		ReportType result = new ObjectFactory().createReportType();
+		for(MonthlyReportBuilder monthlyReportBuilder: monthlyReportBuilders) {
+			result.getMonthlyReport().add(monthlyReportBuilder.toXmlType());
+		}
+		
+		return result;
+	}
+}
