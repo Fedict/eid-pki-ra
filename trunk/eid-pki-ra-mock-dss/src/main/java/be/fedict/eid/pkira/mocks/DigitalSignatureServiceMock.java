30e8f67e7015b73922d945e27473b15b9ebea6e4
hans.vandenbroeck <hans.vandenbroeck@a75cd9ec-122e-11df-83e8-2f775389245c>
2010-04-23 10:50:08 +0000

    Look and feel

--- trunk/eid-pki-ra-mock-dss/src/main/java/be/fedict/eid/pkira/mocks/DigitalSignatureServiceMock.java
+++ trunk/eid-pki-ra-mock-dss/src/main/java/be/fedict/eid/pkira/mocks/DigitalSignatureServiceMock.java
@@ -54,7 +54,7 @@ public class DigitalSignatureServiceMock implements DigitalSignatureServicePortT
 		response.setResult(result);
 
 		NameIdentifierType nameIdentifier = samlObjectFactory.createNameIdentifierType();
-		nameIdentifier.setValue("75033022781");		
+		nameIdentifier.setValue("82010631174");		
 		AnyType any = dssObjectFactory.createAnyType();
 		any.getAny().add(samlObjectFactory.createNameIdentifier(nameIdentifier));
 		response.setOptionalOutputs(any);
