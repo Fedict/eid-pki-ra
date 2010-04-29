7b1bac6ceb1c67c9cf8e611984eb1e79a88c4507
Jan Van den Bergh <janvdbergh@users.noreply.github.com>
2010-04-29 11:06:29 +0000

    Fixed some problems with certificate revocation.

--- eid-pki-ra-portal-model/src/main/java/be/fedict/eid/pkira/portal/ra/certificaterevocation/RevocationContractDssSigningHandler.java
+++ eid-pki-ra-portal-model/src/main/java/be/fedict/eid/pkira/portal/ra/certificaterevocation/RevocationContractDssSigningHandler.java
@@ -1,18 +1,15 @@
 package be.fedict.eid.pkira.portal.ra.certificaterevocation;
 
-import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Scope;
 
 import be.fedict.eid.pkira.contracts.XmlMarshallingException;
 import be.fedict.eid.pkira.generated.contracts.CertificateRevocationResponseType;
 import be.fedict.eid.pkira.portal.ra.AbstractDssSigningHandler;
 
-@Scope(ScopeType.APPLICATION)
 @Name(RevocationContractDssSigningHandler.NAME)
-public class RevocationContractDssSigningHandler 	extends AbstractDssSigningHandler<CertificateRevocationResponseType> {
+public class RevocationContractDssSigningHandler extends AbstractDssSigningHandler<CertificateRevocationResponseType> {
 
-	public static final String NAME = "be.fedict.eid.pkira.revocationContractDssSigningHandler";
+	public static final String NAME = "be.fedict.eid.pkira.portal.revocationContractDssSigningHandler";
 
 	@Override
 	protected String invokeServiceClient(String contract) throws Exception {
