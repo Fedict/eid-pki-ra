4eb7d24a52275c5f292ca04b65202109772b2960
hans.vandenbroeck <hans.vandenbroeck@gmail.com>
2010-07-08 11:04:44 +0000

    to Mock the Context in the tests.

--- eid-pki-ra-blm-model/src/main/java/be/fedict/eid/pkira/blm/model/util/SeamComponentFactory.java
+++ eid-pki-ra-blm-model/src/main/java/be/fedict/eid/pkira/blm/model/util/SeamComponentFactory.java
@@ -1,11 +1,12 @@
 package be.fedict.eid.pkira.blm.model.util;
 
+import org.jboss.seam.Component;
+
 public class SeamComponentFactory extends ComponentFactory {
 
 	@Override
 	public <T> T getComponent(Class<T> componentClass, String name) {
-		// TODO Auto-generated method stub
-		return null;
+		return (T) Component.getInstance(name);
 	}
 
 }
