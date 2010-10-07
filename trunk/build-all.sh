56c0ad185a4cedd2068f95dfc4aa075e43d36715
jan.vdbergh <jan.vdbergh@a75cd9ec-122e-11df-83e8-2f775389245c>
2010-10-07 09:20:41 +0000

    Moved to the POJO implementation of the eID DSS Simple Protocol client.

--- trunk/build-all.sh
+++ trunk/build-all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn clean install && ant deploy-blm && growlnotify -s -m "Build complete" || growlnotify -s -m "Build failed"
+mvn clean install && growlnotify -s -m "Build complete" || growlnotify -s -m "Build failed"
