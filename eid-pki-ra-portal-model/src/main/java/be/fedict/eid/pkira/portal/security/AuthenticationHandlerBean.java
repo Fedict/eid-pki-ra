1ef512d6f009537beab3e49b492913d77ce425ec
Jan Van den Bergh <janvdbergh@users.noreply.github.com>
2011-10-04 13:46:56 +0000

    PKIRA-164 - [Bug] New users: last name field contains first name

--- eid-pki-ra-portal-model/src/main/java/be/fedict/eid/pkira/portal/security/AuthenticationHandlerBean.java
+++ eid-pki-ra-portal-model/src/main/java/be/fedict/eid/pkira/portal/security/AuthenticationHandlerBean.java
@@ -57,7 +57,7 @@ public class AuthenticationHandlerBean extends AbstractAuthenticationHandlerBean
 		if (backendUser != null && backendUser.isWithRegistrations()) {
 			identity.addRole(PKIRARole.REGISTERED_USER.name());
 		} else {
-			eidPKIRAPrivateServiceClient.createRegistrationForUser(eidUser.getRRN(), eidUser.getFirstName(), eidUser.getFirstName(), null, null);
+			eidPKIRAPrivateServiceClient.createRegistrationForUser(eidUser.getRRN(), eidUser.getLastName(), eidUser.getFirstName(), null, null);
 			identity.addRole(PKIRARole.UNREGISTERED_USER.name());
 		}
 		identity.addRole(PKIRARole.AUTHENTICATED_USER.name());
