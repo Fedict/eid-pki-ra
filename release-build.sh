dfad949b671ec69c587ab8cb4bf86ee1bce28889
Jan Van den Bergh <janvdbergh@users.noreply.github.com>
2012-03-26 12:24:17 +0000

    Update of project configuration.

--- release-build.sh
+++
@@ -1,8 +0,0 @@
-mvn clean install -Pproduction,oracle
-mvn clean install -Pproduction,postgres
-
-cd eid-pki-ra-release
-mvn clean install -Poracle
-cp target/eid-pki-ra-release-*.zip ..
-mvn clean install -Ppostgres
-cp target/eid-pki-ra-release-*.zip ..
