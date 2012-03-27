bd2d28e4123cea6d70c3ea0ce5a3d1749f6c1360
jan.vdbergh@gmail.com <jan.vdbergh@gmail.com@a75cd9ec-122e-11df-83e8-2f775389245c>
2012-03-27 06:21:29 +0000

    Switch to version 1.0.12-SNAPSHOT. Update of dev guide.

--- trunk/changeVersion.sh
+++ trunk/changeVersion.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-for i in `find . -name pom.xml` 
+for i in `find . -name pom.xml`
 do
   sed "s/<version>${1}<\/version>/<version>$2<\/version>/" < $i > bla
   mv bla $i
 done
+
+sed "s/${1}/$2/" < eid-pki-ra-dev-guide/src/main/docbook/eid-pki-ra-dev-guide.xml > bla
+mv bla eid-pki-ra-dev-guide/src/main/docbook/eid-pki-ra-dev-guide.xml
